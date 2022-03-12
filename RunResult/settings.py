3a126d4cef367eb322cc7918b84a9d52de74b8f5
hameddavid <reganalyst@yahoo.com>
2022-03-12 21:58:53 +0100

    Testing Oracle Database Connection With Django

--- RunResult/settings.py
+++ RunResult/settings.py
@@ -78,6 +78,16 @@ WSGI_APPLICATION = 'RunResult.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.db.backends.oracle',
+#          'NAME': 'run11g',
+#          'USER': 'pro',
+#          'PASSWORD': 'teewhy',
+#          'HOST': '10.10.10.11',
+#          'PORT': '1521',
+#     }
+# }
 DATABASES = {
     'default': {
          'ENGINE': 'django.db.backends.postgresql',
