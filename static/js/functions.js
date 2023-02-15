02f86e17b0b10700c66fb346240f8d83a3120fc9
hameddavid <reganalyst@yahoo.com>
2023-02-15 12:23:33 +0100

    update score input

--- static/js/functions.js
+++ static/js/functions.js
@@ -131,4 +131,63 @@ $(document).ready(function ($) {
       },
     });
   })
+
+  $("#btnScoreInput").click(function () {
+    $("#inputScoreForm").validate({
+      submitHandler: submitInputScoreForm,
+    });
+
+    function submitInputScoreForm(e) {
+      var formData = $("#inputScoreForm").serialize();
+      var type = "POST";
+      var ajaxurl = "";
+
+      $.ajax({
+        type: type,
+        url: ajaxurl,
+        data: formData,
+        dataType: "json",
+        beforeSend: function () {
+          $("#btnScoreInput").html('<i class="fa fa-spinner fa-spin"></i>');
+          $("#btnScoreInput").prop("disabled", true);
+        },
+        success: function (response) {
+          console.log(response);
+          alert(response.message);
+        },
+        error: function (response) {
+          console.log(response);
+          $("#btnScoreInput").html("Login");
+          $("#btnScoreInput").prop("disabled", false);
+          alert(response.responseJSON.message);
+        },
+      });
+    }
+  });
+
+   $(".scoreTable").on("focusout", ".score", function(){
+    const score = $(this).val()
+    const grade = getGrade(score)
+    const tr = $(this).closest("tr")
+    const current_row = tr.index() + 1
+    $('#'+current_row).val(grade)
+  })
+
+  const getGrade = (score) => {
+    if(score >= 70){
+      return 'A'
+    }
+    else if(score < 70 && score > 59){
+      return 'B'
+    }
+    else if(score < 60 && score > 49){
+      return 'C'
+    }
+    else if(score < 50 && score > 39){
+      return 'D'
+    }
+    else {
+      return 'F'
+    }
+  }
 });
